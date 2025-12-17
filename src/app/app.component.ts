3f78f581326653fc95b5da96ba275671f1a9e782
Akshayaa <akshayaams@symplr.com>
2025-12-17 14:37:05 +0530

    removed nav and dashboard components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,8 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import {NavbarComponent} from './navbar/navbar.component';
-
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, NavbarComponent],
+  imports: [RouterOutlet],
   templateUrl:'./app.component.html',
   standalone:true,
   styleUrls:['../styles.scss'],
