134b05ac71308e95b65b01329d10633001d4210a
Akshayaa <akshayaams@symplr.com>
2025-12-18 19:50:01 +0530

    Dashboard page UI done

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -18,7 +18,39 @@ export class Dashboard {
   { name: 'Sneha', img: this.stuImgPath },
   { name: 'Priya', img: this.stuImgPath },
   { name: 'Karthik', img: this.stuImgPath },
-  { name: 'Ananya', img: this.stuImgPath }
+  { name: 'Ananya', img: this.stuImgPath },
+  { name: 'Abi', img: this.stuImgPath },
+  { name: 'Shakthi', img: this.stuImgPath },
+  { name: 'Bharath', img: this.stuImgPath },
+  { name: 'Soundariya', img: this.stuImgPath },
+  { name: 'Kavya', img: this.stuImgPath },
+  { name: 'Samyuktha', img: this.stuImgPath }
 ];
+
+
+currentPage = 1;
+cardsPerPage = 6;
+
+get paginatedStudents() {
+  const startIndex = (this.currentPage - 1) * this.cardsPerPage;
+  const endIndex = startIndex + this.cardsPerPage;
+  return this.students.slice(startIndex, endIndex);
+}
+
+get totalPages() {
+  return Math.ceil(this.students.length / this.cardsPerPage);
+}
+
+nextPage() {
+  if (this.currentPage < this.totalPages) {
+    this.currentPage++;
+  }
+}
+
+prevPage() {
+  if (this.currentPage > 1) {
+    this.currentPage--;
+  }
+}
 
 }
