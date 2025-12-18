4352de1b9f28346a8b572751189b93c52a11829f
Akshayaa <akshayaams@symplr.com>
2025-12-18 15:27:16 +0530

    dashboard with for

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,11 +1,24 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
 
 @Component({
   selector: 'app-dashboard',
+  imports:[CommonModule],
   standalone: true,
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.scss'],
 })
 export class Dashboard {
   stuImgPath="assets/images/studentImg.png";
+
+  students = [
+  { name: 'Aakash', img: this.stuImgPath },
+  { name: 'Rahul', img: this.stuImgPath },
+  { name: 'Sneha', img: this.stuImgPath },
+  { name: 'Priya', img: this.stuImgPath },
+  { name: 'Karthik', img: this.stuImgPath },
+  { name: 'Ananya', img: this.stuImgPath }
+];
+
 }
