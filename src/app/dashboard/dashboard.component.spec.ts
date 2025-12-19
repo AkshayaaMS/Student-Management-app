038061a87fa9af23224d1ddd6d28e9a070f5226d
Akshayaa <akshayaams@symplr.com>
2025-12-19 14:34:58 +0530

    added dashboard component with delete feature

--- src/app/dashboard/dashboard.component.spec.ts
+++ src/app/dashboard/dashboard.component.spec.ts
@@ -1,23 +1,78 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
-import { Dashboard } from './dashboard.component';
+@Component({
+  selector: 'app-dashboard',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './dashboard.component.html',
+  styleUrls: ['./dashboard.component.scss']
+})
+export class Dashboard {
 
-describe('Dashboard', () => {
-  let component: Dashboard;
-  let fixture: ComponentFixture<Dashboard>;
+  stuImgPath = 'assets/images/studentImg.png';
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [Dashboard]
-    })
-    .compileComponents();
+  students = [
+    { name: 'Aakash', img: this.stuImgPath },
+    { name: 'Rahul', img: this.stuImgPath },
+    { name: 'Sneha', img: this.stuImgPath },
+    { name: 'Priya', img: this.stuImgPath },
+    { name: 'Karthik', img: this.stuImgPath },
+    { name: 'Ananya', img: this.stuImgPath },
+    { name: 'Abi', img: this.stuImgPath },
+    { name: 'Shakthi', img: this.stuImgPath },
+    { name: 'Bharath', img: this.stuImgPath },
+    { name: 'Soundariya', img: this.stuImgPath },
+    { name: 'Kavya', img: this.stuImgPath },
+    { name: 'Samyuktha', img: this.stuImgPath }
+  ];
 
-    fixture = TestBed.createComponent(Dashboard);
-    component = fixture.componentInstance;
-    await fixture.whenStable();
-  });
+  currentPage = 1;
+  cardsPerPage = 6;
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
-});
+  get paginatedStudents() {
+    const startIndex = (this.currentPage - 1) * this.cardsPerPage;
+    return this.students.slice(startIndex, startIndex + this.cardsPerPage);
+  }
+
+  get totalPages() {
+    return Math.ceil(this.students.length / this.cardsPerPage);
+  }
+
+  nextPage() {
+    if (this.currentPage < this.totalPages) {
+      this.currentPage++;
+    }
+  }
+
+  prevPage() {
+    if (this.currentPage > 1) {
+      this.currentPage--;
+    }
+  }
+
+  isModalOpen = false;
+  selectedStudent: any = null;
+
+  openDeleteModal(student: any) {
+    this.selectedStudent = student;
+    this.isModalOpen = true;
+  }
+
+  closeModal() {
+    this.isModalOpen = false;
+    this.selectedStudent = null;
+  }
+
+  confirmDelete() {
+    this.students = this.students.filter(
+      student => student !== this.selectedStudent
+    );
+
+    this.closeModal();
+
+    if (this.currentPage > this.totalPages) {
+      this.currentPage = this.totalPages || 1;
+    }
+  }
+}
--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,56 +1,83 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
-
 @Component({
   selector: 'app-dashboard',
-  imports:[CommonModule],
   standalone: true,
+  imports: [CommonModule],
   templateUrl: './dashboard.component.html',
-  styleUrls: ['./dashboard.component.scss'],
+  styleUrls: ['./dashboard.component.scss']
 })
 export class Dashboard {
-  stuImgPath="assets/images/studentImg.png";
+
+  stuImgPath = 'assets/images/studentImg.png';
 
   students = [
-  { name: 'Aakash', img: this.stuImgPath },
-  { name: 'Rahul', img: this.stuImgPath },
-  { name: 'Sneha', img: this.stuImgPath },
-  { name: 'Priya', img: this.stuImgPath },
-  { name: 'Karthik', img: this.stuImgPath },
-  { name: 'Ananya', img: this.stuImgPath },
-  { name: 'Abi', img: this.stuImgPath },
-  { name: 'Shakthi', img: this.stuImgPath },
-  { name: 'Bharath', img: this.stuImgPath },
-  { name: 'Soundariya', img: this.stuImgPath },
-  { name: 'Kavya', img: this.stuImgPath },
-  { name: 'Samyuktha', img: this.stuImgPath }
-];
+    { name: 'Aakash', img: this.stuImgPath },
+    { name: 'Rahul', img: this.stuImgPath },
+    { name: 'Sneha', img: this.stuImgPath },
+    { name: 'Priya', img: this.stuImgPath },
+    { name: 'Karthik', img: this.stuImgPath },
+    { name: 'Ananya', img: this.stuImgPath },
+    { name: 'Abi', img: this.stuImgPath },
+    { name: 'Shakthi', img: this.stuImgPath },
 
+    { name: 'Bharath', img: this.stuImgPath },
+    { name: 'Soundariya', img: this.stuImgPath },
+    { name: 'Kavya', img: this.stuImgPath },
+    { name: 'Samyuktha', img: this.stuImgPath },
+    { name: 'Deepak', img: this.stuImgPath },
+    { name: 'Meena', img: this.stuImgPath },
+    { name: 'Arjun', img: this.stuImgPath },
+    { name: 'Nithya', img: this.stuImgPath }
+  ];
 
-currentPage = 1;
-cardsPerPage = 6;
+  currentPage = 1;
+  cardsPerPage = 8; 
 
-get paginatedStudents() {
-  const startIndex = (this.currentPage - 1) * this.cardsPerPage;
-  const endIndex = startIndex + this.cardsPerPage;
-  return this.students.slice(startIndex, endIndex);
-}
-
-get totalPages() {
-  return Math.ceil(this.students.length / this.cardsPerPage);
-}
-
-nextPage() {
-  if (this.currentPage < this.totalPages) {
-    this.currentPage++;
+  get paginatedStudents() {
+    const startIndex = (this.currentPage - 1) * this.cardsPerPage;
+    return this.students.slice(startIndex, startIndex + this.cardsPerPage);
   }
-}
 
-prevPage() {
-  if (this.currentPage > 1) {
-    this.currentPage--;
+  get totalPages() {
+    return Math.ceil(this.students.length / this.cardsPerPage);
   }
-}
 
+  nextPage() {
+    if (this.currentPage < this.totalPages) {
+      this.currentPage++;
+    }
+  }
+
+  prevPage() {
+    if (this.currentPage > 1) {
+      this.currentPage--;
+    }
+  }
+
+  isModalOpen = false;
+  selectedStudent: any = null;
+
+  openDeleteModal(student: any) {
+    this.selectedStudent = student;
+    this.isModalOpen = true;
+  }
+
+  closeModal() {
+    this.isModalOpen = false;
+    this.selectedStudent = null;
+  }
+
+  confirmDelete() {
+    this.students = this.students.filter(
+      student => student !== this.selectedStudent
+    );
+
+    this.closeModal();
+
+    if (this.currentPage > this.totalPages) {
+      this.currentPage = this.totalPages || 1;
+    }
+  }
 }
